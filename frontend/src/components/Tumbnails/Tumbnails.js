836b978cfb6072762c599150b654ae057e1e46d4
nina <ninagbs@abv.bg>
2023-10-08 17:13:17 +0300

    listing all pats on home page

--- frontend/src/components/Tumbnails/Tumbnails.js
+++ frontend/src/components/Tumbnails/Tumbnails.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import classes from './tumbnails.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Tumbnails({ pets }) {
     return (
-        <div>Tumbnails</div>
+        <ul className={classes.list}>
+            {pets.map(pet => (
+                <li key={pet.id}>
+                    <Link to={`/pet/${pet.id}`}>
+                        <img
+                            className={classes.image}
+                            src={`${pet.imageUrl}`}
+                            alt={pet.name}
+                        />
+
+                        <div className={classes.content}>
+                            <div className={classes.name}>{pet.name}</div>
+
+                        </div>
+                        <div className={classes.product_item_footer}>
+                            <div className={classes.origins}>
+
+                                <span >{pet.breed}</span>
+
+                            </div>
+                            <div className={classes.cook_time}>
+
+                                Възраст:{pet.age}г.
+                            </div>
+                        </div>
+
+                    </Link>
+                </li>
+            ))}
+        </ul>
     );
-}
+};
